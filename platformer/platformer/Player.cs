bd2627f6cb6082b490e1a546e69cacb85fd40a0a
Alexander Nielsen-titus <alexander.niels@ad.aie.edu>
2016-06-08 18:06:00 +0930

    added initial project
    just the start of the dudgeon and program

---
+++ platformer/platformer/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Platformer
+{
+    class Player
+    {
+
+        Sprite sprite = new Sprite();
+
+        public Player()
+        {
+        }
+        public void Load(ContentManager content)
+        {
+            sprite.Load(content, "hero");
+        }
+        public void Update(float deltaTime)
+        {
+            sprite.Update(deltaTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+    }
+}
